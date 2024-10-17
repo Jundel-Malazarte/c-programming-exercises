cb77ab4b38ee71b4a6feb5a6c8ee8373c6938f64
Jundel Malazarte <jundelmalazarte348@gmail.com>
2024-10-18 02:50:27 +0800

    Inserting the Details

---
+++ Exercises/src/Details.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Name
+    printf("Name: Jundel Malazarte\n");
+
+    // Date of birth
+    printf("DOB: January 29, 2003\n");
+
+    // Age
+    printf("Age: 21\n");
+
+    // Address
+    printf("Address: Cebu City, Phillipines\n");
+
+    // Position
+    printf("Position: Web Developer\n");
+    return 0;
+}
